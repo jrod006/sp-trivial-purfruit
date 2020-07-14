9a80588b6d9af37936df7c9a48093cdeb2e5e198
zzzdragon <mtmanger@gmail.com>
2020-07-14 11:35:48 -0400

    Altering the questio module to move logic out of the constructor and into callable methods

--- tp_question.py
+++ tp_question.py
@@ -3,37 +3,32 @@ import os
 
 class QuestionGenerator:
 
-	def __init__(self):
+        def __init__(self):
+                print('INIT QUESTION GENERATOR')
+                self.df = pd.read_csv('./res/trivial_purfruit_questions.csv')
+                self.colors = list(set(self.df['color'].tolist()))
+                self.categories = {'White': 'Events','Blue': 'Places','Green': 'Independence Day','Red': 'People'}
 
-		print('INIT QUESTION GENERATOR')
-		self.df = pd.read_csv('./res/trivial_purfruit_questions.csv')
-		self.colors = list(set(self.df['color'].tolist()))
-		self.categories = {
-						'White': 'Events',
-						'Blue': 'Places',
-						'Green': 'Independence Day',
-						'Red': 'People'
-					}
+        def askQuestion(self):
+                self.color = input('Enter color to determine question category: ')
+                if self.color in self.colors:
+                        print('================================')
+                        print('Category:', self.categories[self.color])
+                        self.question_df = self.df[self.df['color'] == self.color].copy()
+                        self.loadQA(self.question_df)
+                else:
+                        print('Invalid entry, try again')
 
-		self.color = input('Enter color to determine question category: ')
+        def loadQA(self, df):
 
-		if self.color in self.colors:
-					print('================================')
-					print('Category:', self.categories[self.color])
-					self.question_df = self.df[self.df['color'] == self.color].copy()
-					self.loadQA(self.question_df)
-		else:
-			print('Invalid entry, try again')
-
-	def loadQA(self, df):
-
-		self.random_row = df.sample(n = 1)
-		self.question = self.random_row.iloc[0]['question']
-		self.answer = self.random_row.iloc[0]['answer']
-		print('Question:', self.question)
-		print('Answer:', self.answer)
+                self.random_row = df.sample(n = 1)
+                self.question = self.random_row.iloc[0]['question']
+                self.answer = self.random_row.iloc[0]['answer']
+                print('Question:', self.question)
+                print('Answer:', self.answer)
 
 if __name__ == "__main__":
        
-        QuestionGenerator()
-		
+        QG = QuestionGenerator()
+        QG.askQuestion()
+                
