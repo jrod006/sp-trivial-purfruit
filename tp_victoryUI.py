e255ec594d9f46cea12fbdbb34d4df93f183ace3
Josh Massey <joshua.massey@umbc.edu>
2020-08-18 17:53:03 -0400

    Update to Victory UI functionality

--- tp_victoryUI.py
+++ tp_victoryUI.py
@@ -24,32 +24,32 @@ class Victory:
         self.victoryWindow.title('TP Game Victory')
         self.frame = tk.Frame(self.victoryWindow)
 
-        self.firstPlace = ''' player name '''
-        self.secondPlace = ''' player name '''
-        self.thirdPlace = ''' player name '''
-        self.fourthPlace = ''' player name '''
+        # self.firstPlace = winners[0]
+        # self.secondPlace = winners[1]
+        # self.thirdPlace = winners[2]
+        # self.fourthPlace = winners[3]
 
         # trivial pursuit game results label
         self.victoryHeadLabel = Label(self.victoryWindow, text = 'Game Results', font = arial_bold)
         self.victoryHeadLabel.grid(sticky = 'EW', row = 0, column = 0)
 
-        self.firstPlaceText = "First Place: " + '''first place player'''
+        self.firstPlaceText = "First Place: " + winners[0]
         self.firstPlaceLabel = Label(self.victoryWindow, text = firstPlaceText, font = arial_bold)
         self.firstPlaceLabel.grid(sticky='EW', row=0, column=200)
 
-        self.secondPlaceText = "Second Place: " + '''second place player'''
+        self.secondPlaceText = "Second Place: " + winners[1]
         self.secondPlaceLabel = Label(self.victoryWindow, text=secondPlaceText, font=arial_bold)
         self.secondPlaceLabel.grid(sticky='EW', row=0, column=300)
 
-        if ('''number of players > 2'''):
-            self.thirdPlaceText = "Third Place: " + '''third place player'''
-            self.thirdPlaceLabel = Label(self.victoryWindow, text=thirdPlaceText, font=arial_bold)
-            self.thirdPlaceLabel.grid(sticky='EW', row=0, column=400)
+        # if ('''number of players > 2'''):
+        self.thirdPlaceText = "Third Place: " + winners[2]
+        self.thirdPlaceLabel = Label(self.victoryWindow, text=thirdPlaceText, font=arial_bold)
+        self.thirdPlaceLabel.grid(sticky='EW', row=0, column=400)
 
-        if ('''number of players > 3'''):
-            self.fourthPlaceText = "Fourth Place: " + '''fourth place player'''
-            self.fourthPlaceLabel = Label(self.victoryWindow, text=fourthPlaceText, font=arial_bold)
-            self.fourthPlaceLabel.grid(sticky='EW', row=0, column=500)
+        # if ('''number of players > 3'''):
+        self.fourthPlaceText = "Fourth Place: " + winners[3]
+        self.fourthPlaceLabel = Label(self.victoryWindow, text=fourthPlaceText, font=arial_bold)
+        self.fourthPlaceLabel.grid(sticky='EW', row=0, column=500)
 
     def mainMenu(self):
         print('Close new game settings and return to Main Menu')
